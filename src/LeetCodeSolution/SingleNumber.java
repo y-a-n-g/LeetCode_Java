e2b377f464093ec80580038fe55670fcb55cf240
YY <yy@localusers-MacBook-Pro-7.local>
2014-09-13 01:07:32 -0400

    Solution: Single Number

---
+++ src/LeetCodeSolution/SingleNumber.java
@@ -0,0 +1,22 @@
+package LeetCodeSolution;
+
+//Given an array of integers, every element appears twice except for one. Find that single one.
+
+public class SingleNumber {
+
+	public int singleNumber(int[] A) {
+		int result = A[0];
+		for(int i=1; i<A.length; i++){
+			result = result ^ A[i];
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		SingleNumber caller = new SingleNumber();
+		int[] input = {1,1,2,2,3,3,4,4,5};
+		int result = caller.singleNumber(input);
+		System.out.println(result);
+	}
+
+}
